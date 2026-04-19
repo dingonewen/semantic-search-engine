38b02a5d69e58973861395768ee87fda9d35bb80
Yiwen Ding <dingywn@seas.upenn.edu>
2026-04-18 21:12:11 -0400

    fix ThreadPool hpp file comment misleading part and complete ThreadPool.cpp

--- searchserver/ThreadPool.cpp
+++ searchserver/ThreadPool.cpp
@@ -9,26 +9,52 @@ namespace searchserver {
 // wait for a signal on the work queue condition variable, then they
 // grab work off the queue.  Threads return when they notice that
 // m_killthreads is true.
+// Every worker thread runs forever until being killed
 void ThreadPool::thread_loop() {
-
+  while (true) {
+    Task t;
+    {
+      std::unique_lock<std::mutex> lock(m_mtx);
+      m_cond.wait(lock, [this]{
+        return m_killthreads || !m_work_queue.empty();
+      });  
+      // wake up when m_work_queue is NOT empty or ~ThreadPool() is called
+      if (m_killthreads && m_work_queue.empty()) {
+        return; // ensure threads finish any remaining tasks before dying 
+      }
+      t = m_work_queue.front();
+      m_work_queue.pop_front();
+    } // lock released
+    t.func(t.arg);
+  } 
 }
 
 ThreadPool::ThreadPool(size_t num_threads) : m_thread_vec(), m_mtx(), m_cond(), m_work_queue(), m_killthreads(false) {
-
   // Initialize our member variables.
-
-  // TODO
   // to pass thread_loop to a thread you need to specify the function as ThreadPool::thread_loop
-  // and pass the first argument as `this`
+  // and pass the first argument of thread_loop as `this`
+  for (size_t i = 0; i < num_threads; ++i) {
+    m_thread_vec.emplace_back(&ThreadPool::thread_loop, this);
+  }
 }
 
 ThreadPool:: ~ThreadPool() {
-  
+  {
+    std::scoped_lock<std::mutex> lock(m_mtx);
+    m_killthreads = true;
+  }
+  // wake ALL threads so they can see m_killthreads = true
+  m_cond.notify_all();  
+  // all jthreads auto-join when m_thread_vec is destroyed
 }
 
 // Enqueue a Task for dispatch.
 void ThreadPool::dispatch(Task t) {
-  // TODO
-}
+    {
+      std::scoped_lock<std::mutex> lock(m_mtx);
+      m_work_queue.push_back(t);
+    }
+    m_cond.notify_one();
+  }
 
 }  // namespace searchserver
